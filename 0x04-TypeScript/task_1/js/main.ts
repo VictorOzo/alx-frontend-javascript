a6913d8195157e97a21844f2e801e0a4bf88095f
Ozokwelu Victor <ozokweluvic@gmail.com>
2024-12-09 18:29:23 -0800

    added print function

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -13,7 +13,6 @@ interface Directors extends Teacher {
   numberOfReports: number; // Additional required attribute
 }
 
-// Example usage
 const teacher3: Teacher = {
   firstName: "John",
   fullTimeEmployee: false,
@@ -22,7 +21,6 @@ const teacher3: Teacher = {
   contract: false,
 };
 
-// Example usage
 const director1: Directors = {
   firstName: "John",
   lastName: "Doe",
@@ -31,4 +29,14 @@ const director1: Directors = {
   numberOfReports: 17,
 };
 
-console.log(director1);
+// Define the interface for the function
+interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+
+// Implement the function
+const printTeacher: printTeacherFunction = (firstName, lastName) => {
+  return `${firstName.charAt(0)}. ${lastName}`;
+};
+
+console.log(printTeacher("John", "Doe")); // Output: J. Doe
